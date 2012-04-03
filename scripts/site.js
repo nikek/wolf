054a6ee19c1f4f6e6c44875217dfa76aa8a3e48d
Niklas Ek <ek.niklas@gmail.com>
2012-04-03 07:29:24 +0200

    First commit to backbone, with no backbone, just the script for connecting to socket.

--- scripts/site.js
+++ scripts/site.js
@@ -1,4 +1,48 @@
 
+
+
+// ---------------------------------------------------------
+// Setting up a socket and listening for scoreboard changes
+// ---------------------------------------------------------
+
+(function(){
+	
+	var sb;
+
+	console.log("Try to logon...");
+	var socket = io.connect('http://130.237.8.168:1337');
+
+	// ON CONNECT
+	socket.on("connected", function(data) {
+		console.log("Connected User?", data.accept);
+	});
+
+	// ON RECIEVEFILE
+	socket.on("receiveFile", function(data) {
+
+		if(data !== ""){
+			sb = JSON.parse(data);
+			
+			var teamCount = sb.length;
+			var sbTable = "<tr><th>Team</th><th>Score</th><th>Country</th></tr>";
+
+			for(var i = 0; i<teamCount; i++){
+				sbTable += "<tr>";
+				for(var attr in sb[i]){
+					sbTable += "<td>" + sb[i][attr] + "</td>";
+				}
+				sbTable += "</tr>";
+			}
+
+			$('table#sb').html(sbTable);
+		}else{ console.log("empty data"); }
+	});
+	
+})();
+
+
+
+
 (function ($) {
 
 	$(document).ready(function () {
